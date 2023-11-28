66c87a96094d3a62da4220132ea94ed1166b099c
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-11-28 12:00:41 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (26.66%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,5 +1,15 @@
 class Solution {
     public String defangIPaddr(String address) {
-      return address.replace(".", "[.]");  
+     StringBuilder defangedAddress = new StringBuilder();
+
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                defangedAddress.append("[.]");
+            } else {
+                defangedAddress.append(address.charAt(i));
+            }
+        }
+
+        return defangedAddress.toString();  
     }
 }
