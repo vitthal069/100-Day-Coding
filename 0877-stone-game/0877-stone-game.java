66b4f4eaa173448ed16f35ea3dd66a4b4bfb9d0b
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-09 23:10:55 +0530

    Time: 5 ms (34.22%), Space: 40.4 MB (51.12%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-      return true;
+      int[] dp = new int[piles.length];
+        for(int i = piles.length - 1; i >= 0; i--) {
+            for(int j = i; j < piles.length; j++) {
+                if(i == j) {
+                    dp[i] = piles[i];
+                } else {
+                    dp[j] = Math.max(piles[i] - dp[j], piles[j] - dp[j-1]);
+                }
+            }
+        }
+        return dp[piles.length-1] >= 0;
     }
 }
