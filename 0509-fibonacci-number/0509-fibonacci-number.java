c9d899fb66a08ee5a5f382b51255094921ae131d
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-10 18:15:24 +0530

    Time: 5 ms (47.65%), Space: 38.8 MB (91.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,25 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int a=0,b=1,c=a+b;
-        int count=0,k=0;
-        if(n==1){
-          k=1;
-        }else if(n==2){
-          k=c;
-        }else{
-
-        while(count<=1000){
-            if(count==(n-2))
-            {
-                k=c;
-            }
-            a=b;
-            b=c;
-            c=a+b;
-
-            count++;
+      if(n==0){
+            return 0;
         }
+        if(n==1 || n==2){
+            return 1;
         }
-        return k;
+        return fib(n-1)+fib(n-2);
     }
 }
