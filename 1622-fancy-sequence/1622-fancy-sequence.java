112ffb0c30439a2168b77087c1aa6d153ec85870
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-15 20:37:52 +0530

    Time: 26 ms (100.00%), Space: 76.2 MB (93.10%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.java
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -1,42 +1,76 @@
 class Fancy {
-  // To undo a * val + b and get the original value, we append (val - b) / a.
-  // By Fermat's little theorem:
-  //   a^(p - 1) ≡ 1 (mod p)
-  //   a^(p - 2) ≡ a^(-1) (mod p)
-  // So, (val - b) / a ≡ (val - b) * a^(p - 2) (mod p)
-  public void append(int val) {
-    final long x = (val - b + kMod) % kMod;
-    vals.add(x * modPow(a, kMod - 2) % kMod);
-  }
+    static final int MOD = 1_000_000_007;
+    static final int MAX_CAPACITY = 100_001;
+    static int[] reverseElems = new int[101];
+    static {
+        reverseElems[1] = 1;
+        for (int i = 2; i < reverseElems.length; i++) {
+            reverseElems[i] = (int)fp(i, MOD - 2);
+        }
+    }
 
-  // If the value is a * val + b, then the value after adding by `inc` will be
-  // a * val + b + inc. So, we adjust b to b + inc.
-  public void addAll(int inc) {
-    b = (b + inc) % kMod;
-  }
+    private int addTag = 0;
+    private int multiTag = 1;
+    private int reverseMultiTag = 1;
 
-  // If the value is a * val + b, then the value after multiplying by `m` will
-  // be a * m * val + b * m. So, we adjust a to a * m and b to b * m.
-  public void multAll(int m) {
-    a = (a * m) % kMod;
-    b = (b * m) % kMod;
-  }
+    int[] arr;
+    int size = 0;
 
-  public int getIndex(int idx) {
-    return idx >= vals.size() ? -1 : (int) ((a * vals.get(idx) + b) % kMod);
-  }
+    public Fancy() {
+        arr = new int[MAX_CAPACITY];
+        size = 0;
+    }
+    
+    public void append(int val) {
+        if (size < MAX_CAPACITY) {
+            arr[size++] = calc(val, multiTag, addTag);
+        }
+    }
 
-  private static final int kMod = 1_000_000_007;
-  // For each `val` in `vals`, it actually represents a * val + b.
-  private List<Long> vals = new ArrayList<>();
-  private long a = 1;
-  private long b = 0;
+    private int calc(int v, int r, int d) {
+        v -= d;
+        if (v < 0) {
+            v += MOD;
+        }
+        return (int)(1L * v * reverseMultiTag % MOD);
+    }
 
-  private int modPow(long x, long n) {
-    if (n == 0)
-      return 1;
-    if (n % 2 == 1)
-      return (int) (x * modPow(x % kMod, (n - 1)) % kMod);
-    return modPow(x * x % kMod, (n / 2)) % kMod;
-  }
+    private static long fp(int base, int p) {
+        if (base == 1 || p == 0) {
+            return 1;
+        } else if (p == 1) {
+            return base;
+        }
+        long ans = 1L;
+        long b = base;
+        while (p > 0) {
+            if ((p & 1) == 1) {
+                ans *= b;
+                ans %= MOD;
+            }
+            p >>>= 1;
+            b *= b;
+            b %= MOD;
+        }
+        return ans;
+    }
+    
+    public void addAll(int inc) {
+        addTag += inc;
+        addTag %= MOD;
+    }
+    
+    public void multAll(int m) {
+        multiTag = (int)(1L * multiTag * m % MOD);
+        addTag = (int)(1L * addTag * m % MOD);
+        reverseMultiTag = (int)(1L * reverseMultiTag * reverseElems[m] % MOD);
+    }
+    
+    public int getIndex(int idx) {
+        if (idx >= size) {
+            return -1;
+        }
+        int ans = arr[idx];
+        return (int)((1L * ans * multiTag + addTag) % MOD);
+    }
 }
