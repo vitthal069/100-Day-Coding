246a35670c6e5b69c2defd6b8e66e2adb0cc36e6
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-15 20:38:19 +0530

    Time: 38 ms (82.76%), Space: 100.1 MB (41.38%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.java
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -1,76 +1,55 @@
 class Fancy {
-    static final int MOD = 1_000_000_007;
-    static final int MAX_CAPACITY = 100_001;
-    static int[] reverseElems = new int[101];
-    static {
-        reverseElems[1] = 1;
-        for (int i = 2; i < reverseElems.length; i++) {
-            reverseElems[i] = (int)fp(i, MOD - 2);
-        }
-    }
-
-    private int addTag = 0;
-    private int multiTag = 1;
-    private int reverseMultiTag = 1;
-
-    int[] arr;
-    int size = 0;
-
+    private static final int MOD = 1_000_000_007;
+    private List<Position> list = new ArrayList<>();
     public Fancy() {
-        arr = new int[MAX_CAPACITY];
-        size = 0;
+        list.add(new Position(0));
     }
-    
     public void append(int val) {
-        if (size < MAX_CAPACITY) {
-            arr[size++] = calc(val, multiTag, addTag);
-        }
+        list.add(new Position(val));
     }
-
-    private int calc(int v, int r, int d) {
-        v -= d;
-        if (v < 0) {
-            v += MOD;
-        }
-        return (int)(1L * v * reverseMultiTag % MOD);
-    }
-
-    private static long fp(int base, int p) {
-        if (base == 1 || p == 0) {
-            return 1;
-        } else if (p == 1) {
-            return base;
-        }
-        long ans = 1L;
-        long b = base;
-        while (p > 0) {
-            if ((p & 1) == 1) {
-                ans *= b;
-                ans %= MOD;
-            }
-            p >>>= 1;
-            b *= b;
-            b %= MOD;
-        }
-        return ans;
-    }
-    
     public void addAll(int inc) {
-        addTag += inc;
-        addTag %= MOD;
+        add(list.size() - 1, inc);
     }
-    
     public void multAll(int m) {
-        multiTag = (int)(1L * multiTag * m % MOD);
-        addTag = (int)(1L * addTag * m % MOD);
-        reverseMultiTag = (int)(1L * reverseMultiTag * reverseElems[m] % MOD);
+        mult(list.size() - 1, m);
     }
-    
     public int getIndex(int idx) {
-        if (idx >= size) {
-            return -1;
+        if (idx > list.size() - 2)
+            return -1;   
+        return query(idx);
+    }
+    private int query(int i) {
+        i++;
+        long val = list.get(i).val;
+        while (i < list.size()) {
+            Position p = list.get(i);
+            val = (val * p.mult) % MOD;
+            val = (val + p.add) % MOD;   
+            i += i & -i;
         }
-        int ans = arr[idx];
-        return (int)((1L * ans * multiTag + addTag) % MOD);
+        return (int) val;
+    }
+    private void add(int i, long val) {
+        while (i > 0) {
+            Position p = list.get(i);
+            p.add = (int) ((p.add + val) % MOD);
+            i -= i & -i;
+        }
+    }
+    private void mult(int i, long val) {
+        while (i > 0) {
+            Position p = list.get(i);
+            p.add = (int) ((val * p.add) % MOD);
+            p.mult = (int) ((val * p.mult) % MOD);
+            i -= i & -i;
+        }
+    }
+    class Position {
+        int val;
+        int mult = 1;
+        int add = 0;   
+        Position(int val) {
+            this.val = val;
+        }
     }
 }
