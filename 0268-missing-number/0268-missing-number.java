a9a083fb3a0e40c15952e530e9fbd3de86ec3292
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-17 22:04:31 +0530

    Time: 2 ms (29.92%), Space: 44.4 MB (27.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,30 +1,8 @@
 class Solution {
-    public int missingNumber(int[] arr) {
-        int i = 0;
-        while (i < arr.length) {
-            int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]) {
-                swap(arr, i , correct);
-            } else {
-                i++;
-            }
-        }
-
-        // search for first missing number
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index) {
-                return index;
-            }
-        }
-
-        // case 2
-        return arr.length;
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n*(n+1))/2;
+        int res = sum - Arrays.stream(nums).sum();
+        return res;
     }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }  
-    
 }
