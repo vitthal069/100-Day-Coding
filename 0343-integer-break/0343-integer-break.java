1393099b9bac975d628c6945b6cc4474b52e9d0e
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-18 22:42:10 +0530

    Time: 1 ms (45.41%), Space: 39.4 MB (42.73%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,44 +1,13 @@
 class Solution {
-    public int helper(int n, int idx) {
-        // base condition
-        if (n == 0 || idx == 0)
-            return 1;
-        // recursive call for each step
-        if (idx > n)
-            return helper(n, idx - 1);
-        // return the maximum result obtained from recursive calls
-        return Math.max((idx * helper(n - idx, idx)), helper(n, idx - 1));
-    }
-    public static int tab(int n) {
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+    public int integerBreak(int n) {
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i/2;j++){
+                dp[i]=Math.max(dp[i],j*Math.max(i-j,dp[i-j]));
             }
         }
-
+        // System.out.println(Arrays.toString(dp));
         return dp[n];
-    }
-    public int memo(int n, int idx,int[][] dp) {
-        // base condition
-        if (n == 0 || idx == 0)
-            return 1;
-        if(dp[n][idx]!=-1){ 
-            return dp[n][idx];
-        }
-        // recursive call for each step
-        if (idx > n)
-            return memo(n, idx - 1,dp);
-        // return the maximum result obtained from recursive calls
-        return dp[n][idx]=Math.max((idx * memo(n - idx, idx,dp)), memo(n, idx - 1,dp));
-    }
-    public int integerBreak(int n) {
-        int[][] dp = new int[n+1][n];
-        for(int i=0 ;i<n+1;i++){ 
-            Arrays.fill(dp[i],-1);
-        }
-        return tab(n);
     }
 }
