d9e4692c16a028dcf269f087d2670acf281bf6d0
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-19 21:15:35 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (22.69%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,21 +1,72 @@
 class Solution {
-  public int[][] imageSmoother(int[][] M) {
-    final int m = M.length;
-    final int n = M[0].length;
-    int ans[][] = new int[m][n];
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j) {
-        int ones = 0;
-        int count = 0;
-        for (int y = Math.max(0, i - 1); y < Math.min(m, i + 2); ++y)
-          for (int x = Math.max(0, j - 1); x < Math.min(n, j + 2); ++x) {
-            ones += M[y][x];
-            ++count;
-          }
-        ans[i][j] = ones / count;
-      }
-
-    return ans;
-  }
+    public int[][] imageSmoother(int[][] img) {
+        int rowLast = img.length - 1;
+        int colLast = img[0].length - 1;
+        if (rowLast == 0) {     
+            if (colLast != 0)  singleRow(img[0], colLast);
+        }
+        else if (colLast == 0) {  
+            singleCol(img, rowLast);
+        }
+              else {                    
+            for (int row = 0; row <= rowLast; row++)    
+                sumRow(img[row], colLast);
+            sumCol(img, 0, 4, 6, rowLast);                
+            sumCol(img, colLast, 4, 6, rowLast);         
+            for (int col = 1; col < colLast; col++)     
+                sumCol(img, col, 6, 9, rowLast);
+        }
+        return img;
+    }
+    private void singleRow(int[] MR, int colLast) {
+        int prev = 0;
+        int curr = MR[0];
+        int next = MR[1];
+        MR[0] = (curr + next) / 2;
+        for (int col = 1; col < colLast; col++) {
+            prev = curr;
+            curr = next;
+            next = MR[col+1];
+            MR[col] = (prev + curr + next) / 3;
+        }
+        MR[colLast] = (next + curr) / 2;
+    }
+    private void singleCol(int[][] M, int rowLast) {
+        int prev = 0;
+        int curr = M[0][0];
+        int next = M[1][0];
+        M[0][0] = (curr + next) / 2;
+        for (int row = 1; row < rowLast; row++) {
+            prev = curr;
+            curr = next;
+            next = M[row+1][0];
+            M[row][0] = (prev + curr + next) / 3;
+        }
+        M[rowLast][0] = (next + curr) / 2;
+    }
+    private void sumRow(int[] MR, int colLast) {
+        int prev = 0;
+        int curr = 0;
+        int next = MR[0];
+        for (int col = 0; col < colLast; col++) {
+            prev = curr;
+            curr = next;
+            next = MR[col+1];
+            MR[col] = prev + curr + next;
+        }
+        MR[colLast] = next + curr;
+    }
+    private void sumCol(int[][] M, int col, int endDiv, int midDiv, int rowLast) {
+        int prev = 0;
+        int curr = M[0][col];
+        int next = M[1][col];
+        M[0][col] = (curr + next) / endDiv;
+        for (int row = 1; row < rowLast; row++) {
+            prev = curr;
+            curr = next;
+            next = M[row+1][col];
+            M[row][col] = (prev + curr + next) / midDiv;
+        }
+        M[rowLast][col] = (next + curr) / endDiv;
+}
 }
