f513b8cf7d69a4a186280be51e434e15450553ff
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-19 21:16:08 +0530

    Time: 3 ms (99.16%), Space: 44.6 MB (46.78%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,72 +1,24 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        int rowLast = img.length - 1;
-        int colLast = img[0].length - 1;
-        if (rowLast == 0) {     
-            if (colLast != 0)  singleRow(img[0], colLast);
+        int[][] result = new int[img.length][img[0].length];
+        for(int r=0; r<result.length; r++){
+            for(int c=0; c<result[0].length; c++){
+                result[r][c] = convolution(img,r,c);
+            }
         }
-        else if (colLast == 0) {  
-            singleCol(img, rowLast);
-        }
-              else {                    
-            for (int row = 0; row <= rowLast; row++)    
-                sumRow(img[row], colLast);
-            sumCol(img, 0, 4, 6, rowLast);                
-            sumCol(img, colLast, 4, 6, rowLast);         
-            for (int col = 1; col < colLast; col++)     
-                sumCol(img, col, 6, 9, rowLast);
-        }
-        return img;
+        return result;
     }
-    private void singleRow(int[] MR, int colLast) {
-        int prev = 0;
-        int curr = MR[0];
-        int next = MR[1];
-        MR[0] = (curr + next) / 2;
-        for (int col = 1; col < colLast; col++) {
-            prev = curr;
-            curr = next;
-            next = MR[col+1];
-            MR[col] = (prev + curr + next) / 3;
-        }
-        MR[colLast] = (next + curr) / 2;
+    public int convolution(int[][] img, int row, int col){
+        int sum = img[row][col];
+        int count = 1;
+        if(row-1>=0) {count++;sum += img[row-1][col];}
+        if(row-1>=0 && col-1>=0) {count++;sum += img[row-1][col-1];}
+        if(row-1>=0 && col+1<img[0].length) {count++;sum += img[row-1][col+1];}
+        if(col-1>=0) {count++;sum += img[row][col-1];}
+        if(col+1<img[0].length){count++;sum += img[row][col+1];}
+        if(row+1<img.length && col+1<img[0].length) {count++;sum += img[row+1][col+1];}
+        if(row+1<img.length && col-1>=0) {count++;sum += img[row+1][col-1];}
+        if(row+1<img.length) {count++;sum += img[row+1][col];}
+        return sum/count;
     }
-    private void singleCol(int[][] M, int rowLast) {
-        int prev = 0;
-        int curr = M[0][0];
-        int next = M[1][0];
-        M[0][0] = (curr + next) / 2;
-        for (int row = 1; row < rowLast; row++) {
-            prev = curr;
-            curr = next;
-            next = M[row+1][0];
-            M[row][0] = (prev + curr + next) / 3;
-        }
-        M[rowLast][0] = (next + curr) / 2;
-    }
-    private void sumRow(int[] MR, int colLast) {
-        int prev = 0;
-        int curr = 0;
-        int next = MR[0];
-        for (int col = 0; col < colLast; col++) {
-            prev = curr;
-            curr = next;
-            next = MR[col+1];
-            MR[col] = prev + curr + next;
-        }
-        MR[colLast] = next + curr;
-    }
-    private void sumCol(int[][] M, int col, int endDiv, int midDiv, int rowLast) {
-        int prev = 0;
-        int curr = M[0][col];
-        int next = M[1][col];
-        M[0][col] = (curr + next) / endDiv;
-        for (int row = 1; row < rowLast; row++) {
-            prev = curr;
-            curr = next;
-            next = M[row+1][col];
-            M[row][col] = (prev + curr + next) / midDiv;
-        }
-        M[rowLast][col] = (next + curr) / endDiv;
-}
 }
