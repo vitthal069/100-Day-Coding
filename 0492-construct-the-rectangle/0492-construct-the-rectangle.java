a91bcdb01e4ef31c52aebdb15740989ed62ffc00
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2023-12-26 20:37:20 +0530

    Time: 215 ms (9.67%), Space: 41 MB (5.99%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -1,10 +1,20 @@
 class Solution {
   public int[] constructRectangle(int area) {
-    int width = (int) Math.sqrt(area);
+    int l = 1;
+        int r = 1;
+        int[] result = new int[2];
 
-    while (area % width > 0)
-      --width;
+        for(int i = 1; i <= area; i++) {
+            if (area % i == 0 && (area / i >= i)) {
+                    l = area / i;
+                    r = i;
 
-    return new int[] {area / width, width};
+            }
+        }
+
+        result[0] = l;
+        result[1] = r;
+
+        return result;
   }
 }
