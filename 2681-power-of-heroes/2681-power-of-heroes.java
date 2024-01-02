59df9c70442d9498f3e243e49b129355c710d2ee
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-01-02 20:47:58 +0530

    Time: 34 ms (12.50%), Space: 60.5 MB (5.00%) - LeetHub

--- 2681-power-of-heroes/2681-power-of-heroes.java
+++ 2681-power-of-heroes/2681-power-of-heroes.java
@@ -1,16 +1,25 @@
 class Solution {
-  public int sumOfPower(int[] nums) {
-    final int kMod = 1_000_000_007;
-    long ans = 0;
-    long sum = 0;
-
-    Arrays.sort(nums);
-
-    for (final int num : nums) {
-      ans = (ans + (num + sum) * num % kMod * num % kMod) % kMod;
-      sum = (sum * 2 + num) % kMod;
+    public int sumOfPower(int[] nums) {
+        
+        int mod = 1000000000 + 7 ;
+        
+        int n = nums.length ;
+        
+        Arrays.sort(nums) ;
+        
+        long[] dp = new long[n] ;
+        long[] dpSum  = new long[n] ;
+        long sum = 0 ;
+        for (int i=n-1; i>=0; i--) {
+            
+           dp[i] = (  2 *  ( i+1 >=n ? 0 : dp[i+1] )  + (long) nums[i] * nums[i] ) % mod  ;    
+            long temp = ( i + 1 >= n ? 0 : dp[i+1] ) +   ( (long) nums[i] * nums[i] ) %  mod ;
+            temp = temp * nums[i] % mod ;
+            
+           sum = ( sum +  temp  ) % mod ;
+           //System.out.println(sum + "," + temp) ; 
+        }
+        return (int) sum ;
+        
     }
-
-    return (int) ans;
-  }
 }
