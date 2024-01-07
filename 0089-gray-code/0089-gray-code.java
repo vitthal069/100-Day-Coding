8b2849b2af8fb953c48fc73dfdc4c6d50c3335e2
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-01-07 20:54:04 +0530

    Time: 5 ms (84.44%), Space: 51.5 MB (35.03%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,12 +1,10 @@
 class Solution {
   public List<Integer> grayCode(int n) {
-    List<Integer> ans = new ArrayList<>();
-    ans.add(0);
-
-    for (int i = 0; i < n; ++i)
-      for (int j = ans.size() - 1; j >= 0; --j)
-        ans.add(ans.get(j) | 1 << i);
-
-    return ans;
+   List<Integer> gc = new ArrayList<>();
+        int m = (int)Math.pow(2,n);
+        for(int i=0;i<m;i++){
+            gc.add((i >> 1) ^ i);
+        }
+        return gc;
   }
 }
