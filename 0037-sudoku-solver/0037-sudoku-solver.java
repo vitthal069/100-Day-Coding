65f0b158ef33edc7d416786dc01c2322e842e7d8
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-01-10 20:01:34 +0530

    Time: 15 ms (22.78%), Space: 44.9 MB (5.87%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,34 +1,89 @@
 class Solution {
-  public void solveSudoku(char[][] board) {
-    dfs(board, 0);
-  }
+    private int[][] result = new int[9][9];
+    private boolean[][] modifiable = new boolean[9][9];
 
-  private boolean dfs(char[][] board, int s) {
-    if (s == 81)
-      return true;
+    public void solveSudoku(char[][] board) {
+        markModifiable(board);
+        recurse(0);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.toString(result[i][j]).charAt(0);
+            }
+        }
+    }
 
-    final int i = s / 9;
-    final int j = s % 9;
+    private void markModifiable(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                modifiable[i][j] = (board[i][j] == '.');
+                if (!modifiable[i][j]) {
+                    result[i][j] = Integer.parseInt(String.valueOf(board[i][j]));
+                }
+            }
+        }
+    }
 
-    if (board[i][j] != '.')
-      return dfs(board, s + 1);
+    private boolean recurse(int idx) {
+        if (idx == 81) {
+            return true;
+        }
 
-    for (char c = '1'; c <= '9'; ++c)
-      if (isValid(board, i, j, c)) {
-        board[i][j] = c;
-        if (dfs(board, s + 1))
-          return true;
-        board[i][j] = '.';
-      }
+        int row = idx / 9;
+        int column = idx % 9;
 
-    return false;
-  }
-
-  private boolean isValid(char[][] board, int row, int col, char c) {
-    for (int i = 0; i < 9; ++i)
-      if (board[i][col] == c || board[row][i] == c ||
-          board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+        for (int candidate = 1; candidate < 10; candidate++) {
+            if (modifiable[row][column]) {
+                result[row][column] = candidate;
+                if (isValid(row, column) && recurse(idx + 1)) {
+                    return true;
+                }
+            } else {
+                return recurse(idx + 1);
+            }
+        }
+        result[row][column] = 0;
         return false;
-    return true;
-  }
+    }
+
+    private boolean isValid(int i, int j) {
+        return isValidRow(i) && isValidColumn(j) && isValidBlock(i, j);
+    }
+
+    private boolean isValidRow(int i) {
+        boolean[] seen = new boolean[10];
+        for (int j = 0; j < 9; j++) {
+            int value = result[i][j];
+            if (seen[value] && value != 0) {
+                return false;
+            }
+            seen[value] = true;
+        }
+        return true;
+    }
+
+    private boolean isValidColumn(int j) {
+        boolean[] seen = new boolean[10];
+        for (int i = 0; i < 9; i++) {
+            int value = result[i][j];
+            if (seen[value] && value != 0) {
+                return false;
+            }
+            seen[value] = true;
+        }
+        return true;
+    }
+
+    private boolean isValidBlock(int i, int j) {
+        boolean[] seen = new boolean[10];
+        for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++) {
+            for (int column = (j / 3) * 3; column < (j / 3) * 3 + 3; column++) {
+                int value = result[row][column];
+                if (seen[value] && value != 0) {
+                    return false;
+                }
+                seen[value] = true;
+           }
+        }
+        return true;
+    }
 }
