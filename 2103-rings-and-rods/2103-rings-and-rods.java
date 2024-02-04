cdaecb518305cd831fbcf3d29b5107d395d006da
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-02-04 23:10:52 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (7.36%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,14 +1,23 @@
 class Solution {
-    public int countPoints(String rings) {
+    public int countPoints(String rings)
+    {
         int[][] count = new int[26][10];
-        for (int i = 0; i + 1 < rings.length(); i = i + 2) {
+        
+        for (int i = 0; i + 1 < rings.length(); i = i + 2)
+        {
             int row = rings.charAt(i) - 'A';
+            
             int col = rings.charAt(i + 1) - '0';
+            
             count[row][col]++;
         }
+        
         int res = 0;
-        for (int i = 0; i < 10; i++) {
-            if (count['B' - 'A'][i] > 0 && count['G' - 'A'][i] > 0 && count['R' - 'A'][i] > 0) {
+        
+        for (int i = 0; i < 10; i++) 
+        {
+            if (count['B' - 'A'][i] > 0 && count['G' - 'A'][i] > 0 && count['R' - 'A'][i] > 0)
+            {
                 res++;
             }
         }
