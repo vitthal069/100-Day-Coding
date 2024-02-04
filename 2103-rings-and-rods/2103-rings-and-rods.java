f27080a73bd4e08cd631d0f50de35369ec65aee8
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-02-04 23:10:14 +0530

    Time: 0 ms (100.00%), Space: 41 MB (74.75%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,13 +1,17 @@
 class Solution {
-  public int countPoints(String rings) {
-    int[] colors = new int[10];
-
-    for (int i = 0; i < rings.length(); i += 2) {
-      final char c = rings.charAt(i);
-      final int color = c == 'R' ? 1 : c == 'G' ? 2 : 4;
-      colors[rings.charAt(i + 1) - '0'] |= color;
+    public int countPoints(String rings) {
+        int[][] count = new int[26][10];
+        for (int i = 0; i + 1 < rings.length(); i = i + 2) {
+            int row = rings.charAt(i) - 'A';
+            int col = rings.charAt(i + 1) - '0';
+            count[row][col]++;
+        }
+        int res = 0;
+        for (int i = 0; i < 10; i++) {
+            if (count['B' - 'A'][i] > 0 && count['G' - 'A'][i] > 0 && count['R' - 'A'][i] > 0) {
+                res++;
+            }
+        }
+        return res;
     }
-
-    return (int) Arrays.stream(colors).filter(c -> c == 7).count();
-  }
 }
