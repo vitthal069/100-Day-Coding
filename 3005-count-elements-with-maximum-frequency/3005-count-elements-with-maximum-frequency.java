5f5592a313ac43c1182fd8195290e7436f483ff2
Vitthal Dubey <119072000+vitthal069@users.noreply.github.com>
2024-03-08 20:09:32 +0530

    Time: 1 ms (99.64%), Space: 42.1 MB (79.05%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,18 +1,28 @@
 class Solution {
   public int maxFrequencyElements(int[] nums) {
-    final int kMax = 100;
-    int ans = 0;
-    int[] count = new int[kMax + 1];
+    int maxNum = Integer.MIN_VALUE;
+      for(int num:nums){
+         if(num>maxNum) maxNum = num;
+      }
+       
+       int arr[] = new int[maxNum+1];
+       for(int num2:nums){
+           arr[num2]++;
+       }
 
-    for (final int num : nums)
-      ++count[num];
+        int maxFrequency = 0;
+        for (int freq : arr) {
+            if (freq > maxFrequency) {
+                maxFrequency = freq;
+            }
+        }
 
-    final int maxFreq = Arrays.stream(count).max().getAsInt();
-
-    for (final int freq : count)
-      if (freq == maxFreq)
-        ans += maxFreq;
-
-    return ans;
+         int countMaxFrequency = 0;
+        for (int freq : arr) {
+            if (freq == maxFrequency) {
+                countMaxFrequency++;
+            }
+        }
+    return maxFrequency*countMaxFrequency;
   }
 }
